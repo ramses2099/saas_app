f1c4a6f12df652c331e8b33e12719cba2336c167
ramses2099 <ramses2099@gmail.com>
2024-12-20 12:41:24 -0500

    last changes

--- src/core/views.py
+++ src/core/views.py
@@ -1,15 +1,21 @@
 import pathlib
 
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
+    # queryset = PageVisit.objects.all()
+    queryset = PageVisit.objects.filter(path=request.path)
+
     title = "Home Page"
     context = {
         "title": title,
+        "visit_count": queryset.count(),
     }
+    PageVisit.objects.create(path=request.path)
     html_template = "home.html"
     return render(request, html_template,context=context)
 
